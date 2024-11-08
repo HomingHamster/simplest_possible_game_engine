371ec91b4b85100d7447d237f46b27781d27f957
Newton <newton@audit.best>
2024-11-08 20:08:04 +0000

    readme updates

---
+++ README.md
@@ -0,0 +1,29 @@
+(c) 2024 Felix Farquharson
+This project is licensed under MIT license (see LICENSE.txt for details)
+
+SIMPLEST POSSIBLE GAME ENGINE IN PYTHON
+=======================================
+
+This project is the simplest possible 2D game engine deigned as a
+educational exercise, you can move a text based character around
+a text based grid of any size.
+
+Sponsorship
+-----------
+Please give me money:
+
+BTC: bc1qwm3rswv0xcunchw9q72mnsmxjzkhfdkwr0vlgu
+
+LTC: LdRc7Fz6poAuWvXexzGP4sQ7SFvn6uPuuX
+
+Card: https://buy.stripe.com/00g00m6e1gEW68E4gk
+
+How to install/run
+------------------
+
+You can see this code running online at this link 
+https://trinket.io/python3/1a47584e39ee
+
+You can also download the main.py file or copy the contents and use
+the python interpreter locally. You will need to install python
+and then open the command prompt and type "python path/to/file.py".
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+# (c) 2024 Felix Farquharson
+# This code is licensed under MIT license (see LICENSE.txt for details)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# SIMPLEST POSSIBLE GAME ENGINE IN PYTHON
 
+# Please give me money:
+# BTC: bc1qwm3rswv0xcunchw9q72mnsmxjzkhfdkwr0vlgu
+# LTC: LdRc7Fz6poAuWvXexzGP4sQ7SFvn6uPuuX
+# Card: https://buy.stripe.com/00g00m6e1gEW68E4gk
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+character_x = 0  # Initialize character's X position
+character_y = 0  # Initialize character's Y position
+screen_size_x = 10  # Define screen width
+screen_size_y = 10  # Define screen height
 
+# Function to render the game screen
+def render_screen():
+    for y in range(screen_size_y): #loop over the number of lines
+        for x in range(screen_size_x): #loop over the number of pixels in that line
+            if character_y == y and character_x == x:
+                print("X", end="")  # Highlight character's position with "X"
+            else:
+                print(".", end="")  # Display empty cells with "."
+        print("")  # Move to the next row
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Main game loop
+while True:
+    render_screen()  # Render the game screen
+    key = input("Direction to move: ")  # Capture user input for movement
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Update character's position based on user input
+    if key == "w":
+        character_y -= 1  # Move character up
+    elif key == "a":
+        character_x -= 1  # Move character left
+    elif key == "s":
+        character_y += 1  # Move character down
+    elif key == "d":
+        character_x += 1  # Move character right
+    elif key == "q":
+        break  # Exit the game if 'q' is pressed
